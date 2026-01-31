4888fa0b9490368ca9d3692e6449bbdb3c4606a7
Syed Maroof <syedmarhuss@gmail.com>
2026-01-31 18:47:55 -0500

    init springboot

---
+++ backend/filespark-server/src/main/java/com/filespark/filespark_server/FilesparkServerApplication.java
@@ -0,0 +1,15 @@
+package com.filespark.filespark_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesparkServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FilesparkServerApplication.class, args);
+		
+	}
+
+}
---
+++ backend/filespark-server/src/test/java/com/filespark/filespark_server/FilesparkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filespark.filespark_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesparkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
