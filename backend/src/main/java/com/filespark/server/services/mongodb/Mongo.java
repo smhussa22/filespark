d1f9b4879a701c9abcc03be845578a7c7d617ed6
Syed Maroof <syedmarhuss@gmail.com>
2026-02-01 21:31:17 -0500

    mongo and mime

--- backend/src/main/java/com/filespark/server/services/mongodb/Mongo.java
+++ backend/src/main/java/com/filespark/server/services/mongodb/Mongo.java
@@ -1,5 +1,29 @@
 package com.filespark.server.services.mongodb;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+@Configuration
 public class Mongo {
+
+    @Bean
+    public MongoClient client (@Value("${spring.data.mongodb.uri}") String mongoUri) {
+
+        return MongoClients.create(mongoUri);
+
+    }
+
+
+    @Bean
+    public MongoDatabase database (MongoClient client, String dbName) {
+
+        return client.getDatabase(dbName);
+
+    }
     
 }
--- backend/src/main/java/com/filespark/server/util/Mime.java
+++ backend/src/main/java/com/filespark/server/util/Mime.java
@@ -1,5 +1,49 @@
 package com.filespark.server.util;
 
+import java.util.Map;
+
+import org.springframework.util.InvalidMimeTypeException;
+
 public class Mime {
+
+    private static final Map<String, String> mimeMap = Map.ofEntries(
+
+        Map.entry("image/png", "png"),
+        Map.entry("image/jpeg", "jpg"),  
+        Map.entry("image/webp", "webp"),
+        Map.entry("image/heic", "heic"),
+
+        Map.entry("video/mp4", "mp4"),
+        Map.entry("video/quicktime", "mov"), 
+        Map.entry("video/x-matroska", "mkv"), 
+
+        Map.entry("application/pdf", "pdf"),
+        Map.entry("application/vnd.openxmlformats-officedocument.wordprocessingml.document", "docx"), 
+        Map.entry("application/zip", "zip"),
+        Map.entry("application/x-rar-compressed", "rar"),
+        Map.entry("application/x-7z-compressed", "7z"),
+
+        Map.entry("text/plain", "txt")
+
+    );
+
+    public static String getExtensionFromMime(String mime) {
+
+        if (mime == null || mime.isBlank()) { 
+
+            throw new InvalidMimeTypeException(mime, "com.filespark.server.util.Mime: MIME type not found.");
+
+        }
+
+        String extension = mimeMap.get(mime.toLowerCase());
+        if (extension == null) { 
+
+            throw new InvalidMimeTypeException(mime, "com.filespark.server.util.Mime: MIME type not supported.");
+
+        }
+
+        return extension;
+
+    }
     
 }
